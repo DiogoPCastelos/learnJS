beb9b77ee9fab9fad8a0333b078037c9789a5c7f
Diogo Piteira Castelos <main@diogopcastelos.pt>
2024-02-21 12:09:11 +0000

    End Web Tutorial 1

---
+++ Project 1/index.js
@@ -0,0 +1,12 @@
+// Performing a task
+function greet(firstName, lastName) {
+  console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+greet('John', 'Doe');
+console.log(square(2));
